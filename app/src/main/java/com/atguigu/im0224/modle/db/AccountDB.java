12273c51a0bc2b293433bc34cda9b21b46e21f0f
菜鸟大叔 <418593670@qq.com>
2017-07-01 14:50:15 +0800

    CRUD

---
+++ app/src/main/java/com/atguigu/im0224/modle/db/AccountDB.java
@@ -0,0 +1,28 @@
+package com.atguigu.im0224.modle.db;
+
+/**
+ * Created by Administrator on 2017/7/1.
+ */
+
+/*
+*
+create table student(id text primary key,name text)
+
+insert into student(id,name) values('10','小福')
+
+insert into student(name) values('小福')
+
+insert into student(id,name) values('1','小仓')
+
+insert into student(id,name) values('2','志玲')
+
+update student set  id = '3' where name = '志玲'
+
+select name from student where id = '1'
+
+delete from student where id = '1'
+*
+* */
+public class AccountDB {
+    
+}
