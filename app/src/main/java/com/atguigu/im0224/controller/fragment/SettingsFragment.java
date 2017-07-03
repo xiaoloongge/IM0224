ee4a7131c92267f1a831816cc1eda80089db970a
菜鸟大叔 <418593670@qq.com>
2017-07-03 09:33:10 +0800

    主界面的实现

--- app/src/main/java/com/atguigu/im0224/controller/fragment/SettingsFragment.java
+++ app/src/main/java/com/atguigu/im0224/controller/fragment/SettingsFragment.java
@@ -1,12 +1,22 @@
 package com.atguigu.im0224.controller.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+
+import com.atguigu.im0224.R;
+import com.atguigu.im0224.controller.activity.LoginActivity;
+import com.atguigu.im0224.controller.activity.MainActivity;
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2017/7/3.
@@ -14,11 +24,63 @@ import android.widget.TextView;
 
 public class SettingsFragment extends Fragment {
 
+    @Bind(R.id.setting_btn_exit)
+    Button settingBtnExit;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText("bbb");
-        return textView;
+
+        View view = View.inflate(getActivity(), R.layout.fragment_setttings, null);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //获取当前用户 1 数据库 2 环信api
+        String currentUser = EMClient.getInstance().getCurrentUser();
+        settingBtnExit.setText("退出登录（"+currentUser+")");
+
+        settingBtnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                /*
+                * 第一个参数 如果集成了GCM等第三方推送，方法里第一个参数需要设为true
+                * */
+                EMClient.getInstance().logout(false, new EMCallBack() {
+                    @Override
+                    public void onSuccess() {
+                        //清除个人数据
+                        //跳转到登录界面
+                        startActivity(new Intent(getActivity(), LoginActivity.class));
+                        //把当前界面finsh
+                        getActivity().finish();
+                    }
+
+                    @Override
+                    public void onError(int i, String s) {
+
+                    }
+
+                    @Override
+                    public void onProgress(int i, String s) {
+
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
